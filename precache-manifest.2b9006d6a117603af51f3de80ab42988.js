b8e54bd01b48ed450d3146c20409d4475e276b68
Arek <arkadiusz.pabisz@gmail.com>
2021-06-17 07:32:10 +0200

    fix: changed routes for github

---
+++ precache-manifest.2b9006d6a117603af51f3de80ab42988.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5893b27fa325589cf1d",
+    "url": "/react-app-1-rr/static/css/main.e7771c58.chunk.css"
+  },
+  {
+    "revision": "a5893b27fa325589cf1d",
+    "url": "/react-app-1-rr/static/js/main.54c357c4.chunk.js"
+  },
+  {
+    "revision": "fa0f997496378cfeb004",
+    "url": "/react-app-1-rr/static/js/runtime~main.8c75b768.js"
+  },
+  {
+    "revision": "773438002362cc0588cb",
+    "url": "/react-app-1-rr/static/js/2.0389827c.chunk.js"
+  },
+  {
+    "revision": "f2e5ef941e887f614c1cdd83eb891f79",
+    "url": "/react-app-1-rr/static/media/header1.f2e5ef94.jpg"
+  },
+  {
+    "revision": "5ea61e1d637c53d4ff46f1d1f835a9e7",
+    "url": "/react-app-1-rr/static/media/header2.5ea61e1d.jpg"
+  },
+  {
+    "revision": "dc97275545555758363094b00aee25e7",
+    "url": "/react-app-1-rr/static/media/header3.dc972755.jpg"
+  },
+  {
+    "revision": "299a7c773da5f783acd0a4510a970771",
+    "url": "/react-app-1-rr/static/media/header4.299a7c77.jpg"
+  },
+  {
+    "revision": "edbcec5e887defefa3760732a44c1d11",
+    "url": "/react-app-1-rr/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-app-1-rr/precache-manifest.2b9006d6a117603af51f3de80ab42988.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-app-1-rr/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
